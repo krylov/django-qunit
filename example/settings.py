43cd37d12a71625bf2b4236d77b03c7f6d0e3df5
Ilya Krylov <krylov@corp.sputnik.ru>
2017-01-25 15:01:17 +0300

    Added new settings in the example: QUNIT_PACKAGE_JSON and QUNIT_INDEX_HTML

--- example/settings.py
+++ example/settings.py
@@ -2,15 +2,18 @@ import os
 
 BASE_PATH = os.path.dirname(__file__)
 
-QUNIT_TEST_DIRECTORY = os.path.join(BASE_PATH, 'qunit_tests')
+QUNIT_TEST_DIRECTORY = os.path.join(BASE_PATH, "qunit_tests")
+QUNIT_PACKAGE_JSON = "package.json"
+QUNIT_INDEX_HTML = "tests.html"
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
-ROOT_URLCONF = 'example.urls'
+ROOT_URLCONF = "example.urls"
 
 INSTALLED_APPS = (
-    'django_qunit',
+    "django_qunit",
 )
 
-SECRET_KEY = 'qwerty'
+SECRET_KEY = "qwerty"
+TEMPLATE_DIRS = [QUNIT_TEST_DIRECTORY]
